20cd81be36f474bfd87fd2b154d55362d4f344de
Badered10 <badr8aouragh@gmail.com>
2025-11-11 14:27:16 +0100

    Add need tsconfig and add some practical examples

--- Lesson00/main.js
+++ Lesson00/main.js
@@ -1,2 +1,15 @@
-var username = 'HELLO WORLD 10';
-console.log(username);
+var str = "";
+str = "HELLO WORLD";
+function printMessage() {
+    console.log(str);
+}
+try {
+    printMessage();
+    throw new Error("Somthing went wrong");
+}
+catch (err) {
+    if (err instanceof Error)
+        console.error("Error message: ", err);
+    else
+        console.error("Unknown error:", err);
+}
--- Lesson00/main.ts
+++ Lesson00/main.ts
@@ -1,3 +1,23 @@
-let username = 'HELLO WORLD 10'
 
-console.log(username)
+var str : string = "";
+
+str = "HELLO WORLD"
+
+function printMessage(): void
+{
+    console.log(str);
+}
+
+try
+{
+    printMessage();
+    throw new Error("Somthing went wrong");
+}
+
+catch (err)
+{
+    if (err instanceof Error)
+        console.error("Error message: ", err);
+    else
+        console.error("Unknown error:", err);
+}
